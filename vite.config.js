c59030cb9f472c18246d5ae34940ed656f3de64b
Slayko <mansurkenzetaj62@gmail.com>
2024-12-29 20:54:45 +0500

    ersr

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://your-username.github.io/your-repository-name/',
+  base: 'https://github.com/leazermon21as/AnimeWord.git/',
 })
